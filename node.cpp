7a3aed6903164f657b0c7fdf90c960c94a26359d
Strelok11197 <dima_vibe_97@mail.ru>
2019-10-13 19:42:28 +0300

    first commit

---
+++ node.cpp
@@ -0,0 +1,24 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+int main(int args, char** argv)
+{
+	ros::init(int argc, argv, "node");
+	ros::NodeHandle n;
+	ros::Publisher pub = n.advertise<std_msgs::String>("/topic", 1000);
+
+	ros::Rate loop_rate(1);
+	int a;
+	while (cin >> a)
+	{
+		std_msgs::String msg;
+
+		msg.data = a;
+
+		ROS_INFO("%s", msg.data.c_str());
+		pub.publish(msg);
+		loop_rate.sleep();
+	}
+	ros::spinOnce();
+	return 0;
+}
